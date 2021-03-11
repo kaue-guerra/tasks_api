5cc88df35ede229bac25f601eb8c7a652e5ab99e
Kaue <kaueguerra@gmail.com>
2021-03-11 08:43:41 -0300

    Ädicionado o cors no projeto

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import { createConnection } from 'typeorm';
+import * as cors from 'cors';
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { routes } from "./routes";
 
+
 const app = express();
 createConnection();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(routes)
 
